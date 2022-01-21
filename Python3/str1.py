4ab2099daf4ab601c16caad26a646070cfbf0e87
ryu57sp <ryuki.baske5@gmail.com>
2022-01-21 20:36:15 +0900

    特殊メソッド

---
+++ Python3/str1.py
@@ -0,0 +1,6 @@
+class Point:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+
+print(Point(1, 2))
---
+++ Python3/str2.py
@@ -0,0 +1,9 @@
+class Point:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+
+print(Point(1, 2))
